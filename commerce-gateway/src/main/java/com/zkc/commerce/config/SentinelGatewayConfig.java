ab603e0c82ef8d18463bf3874680f3d97151a737
kcTest <763336180@qq.com>
2023-05-27 16:09:16 +0800

    sentinel custom BlockRequestHandler test

--- commerce-gateway/src/main/java/com/zkc/commerce/config/SentinelGatewayConfig.java
+++ commerce-gateway/src/main/java/com/zkc/commerce/config/SentinelGatewayConfig.java
@@ -4,6 +4,8 @@ import com.alibaba.csp.sentinel.adapter.gateway.common.SentinelGatewayConstants;
 import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
 import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
 import jakarta.annotation.PostConstruct;
@@ -14,20 +16,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.reactive.result.view.ViewResolver;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * gateway+sentinel 限流
  * <p>
  * https://github.com/alibaba/Sentinel/wiki/%E7%BD%91%E5%85%B3%E9%99%90%E6%B5%81
  * <p>
- * ??
+ * 使用默认DefaultBlockRequestHandler ??
  * java.lang.NoSuchMethodError: 'org.springframework.web.reactive.function.server
  * .ServerResponse$BodyBuilder org.springframework.web.reactive.function.server.ServerResponse.
  * status(org.springframework.http.HttpStatus)'
@@ -75,6 +80,8 @@ public class SentinelGatewayConfig {
 		log.info("============================");
 		log.info("加载sentinel网关限流规则");
 		initGatewayRules();
+		log.info("加载自定义sentinel网关限流处理器");
+		initBlockHandlers();
 		log.info("============================");
 	}
 	
@@ -100,5 +107,33 @@ public class SentinelGatewayConfig {
 		rules.add(flowRule);
 		
 		GatewayRuleManager.loadRules(rules);
+	}
+	
+	/**
+	 * 自定义限流异常处理器
+	 * <p>
+	 * 在 GatewayCallbackManager 注册回调进行定制：
+	 * <p>
+	 * setBlockHandler：注册函数用于实现自定义的逻辑处理被限流的请求，对应接口为 BlockRequestHandler。
+	 * 默认实现为 DefaultBlockRequestHandler，当被限流时会返回类似于下面的错误信息：Blocked by Sentinel: FlowException。
+	 */
+	private void initBlockHandlers() {
+		BlockRequestHandler blockRequestHandler = new BlockRequestHandler() {
+			@Override
+			public Mono<ServerResponse> handleRequest(ServerWebExchange exchange, Throwable t) {
+				log.error("===============触发sentinel网关限流规则");
+				
+				Map<String, String> result = new HashMap<>();
+				result.put("code", String.valueOf(HttpStatus.TOO_MANY_REQUESTS.value()));
+				result.put("message", HttpStatus.TOO_MANY_REQUESTS.getReasonPhrase());
+				result.put("route", "commerce-nc");
+				
+				return ServerResponse.status(HttpStatus.TOO_MANY_REQUESTS)
+						.contentType(MediaType.APPLICATION_JSON)
+						.body(BodyInserters.fromValue(result));
+			}
+		};
+		
+		GatewayCallbackManager.setBlockHandler(blockRequestHandler);
 	}
 }
